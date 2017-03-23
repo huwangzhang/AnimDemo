c26c8f1b2be09d80bdbe4e84fa4c3b58282d2f73
huwang <windang@163.com>
2017-03-23 20:34:39 +0800

    逐帧动画

--- app/src/main/java/com/example/huwang/animdemo/MainActivity.java
+++ app/src/main/java/com/example/huwang/animdemo/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.huwang.animdemo;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    AnimationDrawable am;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.view);
+        am = (AnimationDrawable)imageView.getBackground();
+    }
+    public void play(View view) {
+        am.start();
+    }
+    public void stop(View view) {
+        am.stop();
     }
 }
